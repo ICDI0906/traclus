81e3d1e489ff5cdd80240c6afd1a5834cecc4a63
ICDI0906 <18217060267@163.com>
2018-12-17 13:16:52 +0800

    approximate_trajectory_partitioning

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include "Segment.h"
 #include "Segment.cpp"
 #include <algorithm>
-
+#include <string>
+#include <vector>
 using namespace std;
 /*
  * 测试一下距离公式
@@ -16,7 +17,53 @@ void test_dist(){
 //    cout << min(1,2);/**/
     cout << si.perpendicular_dist(sj) << "  " << si.parallel_dist(sj) <<" "<<si.angle_dist(sj)<< endl;
 }
+vector<Point> seq;
+double MDL(int start_index,int currIndex,string t){
+    Segment se = Segment(seq[start_index],seq[currIndex]);
+    double l_h = 0.0,l_d_h = 0.0;
+    if(t == "par") {
+        l_h = log2(se.getSegmentLength());
+    }
+    for(int i = start_index ; i < currIndex;i++){
+        Segment sub = Segment(seq[i], seq[i + 1]);
+        if(t == "par") {
+            l_d_h += se.perpendicular_dist(sub);
+            l_d_h += se.angle_dist(sub);
+        }
+        else if (t == "nopar"){
+            l_h += sub.getSegmentLength();
+        }
+    }
+    if(t == "par"){
+        return l_h + l_d_h;
+    }
+    else{
+        return l_h;
+    }
+}
+void approximate_trajectory_partitioning(){
+    int start_index = 0,length = 1;
+    cout << "0"<<endl;
+    while(start_index + length < seq.size()){
+        int currIndex = start_index + length;
+        double cost_par = MDL(start_index,currIndex,"par");
+        double cost_nopar = MDL(start_index,currIndex,"nopar");
+        cout << "start_index：" <<start_index << "  currIndex: "<< currIndex <<" "<<cost_par << "  par and nopar  "<< cost_nopar<<endl;
+        if(cost_par > cost_nopar){
+            cout << "index : "<< currIndex - 1 << endl;
+            start_index = start_index + length;
+        }
+        else{
+            length ++;
+        }
+    }
+}
 int main() {
-
+    seq.push_back(Point(1,1));
+    seq.push_back(Point(2,2));
+    seq.push_back(Point(3,2.5));
+    seq.push_back(Point(4,1));
+    seq.push_back(Point(5,0.5));
+    approximate_trajectory_partitioning();
     return 0;
 }
