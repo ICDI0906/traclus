0cfd2047293f99c03e48969478346345400ff590
ICDI0906 <18217060267@163.com>
2018-12-19 23:05:32 +0800

    基本实现啦还有图

--- main.cpp
+++ main.cpp
@@ -16,8 +16,10 @@ set<Segment*> partitionDB; // 线段的集合
 map<int,set<int>> cluster_cnt;
 vector<vector<Point*>> representive_point;
 vector<vector<Segment*>> seg_global;
-double eplison = 10;
-int minLins = 20;
+double eplison = 35;
+int minLins = 8;
+int min_traj = 8;
+double min_dist = 10;
 /*
  * 测试一下距离公式
  */
@@ -29,7 +31,6 @@ void test_dist(){
 //    cout << min(1,2);/**/
 //    cout << si.perpendicular_dist(sj) << "  " << si.parallel_dist(sj) <<" "<<si.angle_dist(sj)<< endl;
 }
-
 double MDL(int traj_id, int start_index,int currIndex,string t){
     Segment se = Segment(db[traj_id][start_index],db[traj_id][currIndex],traj_id);
     double l_h = 0.0,l_d_h_l = 0.0,l_d_h_r = 0.0;
@@ -73,23 +74,26 @@ void approximate_trajectory_partitioning(){
             }
         }
     }
-    cout << partitionDB.size() << "size :" <<endl;
+//    cout << partitionDB.size() << "size :" <<endl;
 }
 
-set<Segment*> n_eplison_l(Segment seg){
+set<Segment*> n_eplison_l(Segment *seg){
     set<Segment*> tmp;
-    Segment seg_i,seg_j;
+    Segment *seg_i,*seg_j;
     for(Segment *seg_tmp: partitionDB){ // 取较长的
-        if(seg_tmp->getSegmentLength() > seg.getSegmentLength()){
-            seg_i = *seg_tmp;
+        if(seg == seg_tmp){
+            continue;
+        }
+        if(seg_tmp->getSegmentLength() > seg->getSegmentLength()){
+            seg_i = seg_tmp;
             seg_j = seg;
         }
         else{
             seg_i = seg;
-            seg_j = *seg_tmp;
+            seg_j = seg_tmp;
         }
-        if(seg_i.getAllDistance(seg_j) <= eplison){
-            cout <<"all distance: "<< seg_i.getAllDistance(seg_j) << " "<<endl;
+        if(seg_i->getAllDistance(*seg_j) <= eplison){
+//            cout <<"all distance: "<< seg_i->getAllDistance(*seg_j) << " "<<endl;
             tmp.insert(seg_tmp);
         }
     }
@@ -97,13 +101,13 @@ set<Segment*> n_eplison_l(Segment seg){
 }
 
 void expand_cluster(queue<Segment*> que,int cluster_id){
-    cout<< "start  -------- "<<endl;
+//    cout<< "start  -------- "<<endl;
     while(!que.empty()){
 
         Segment * front = que.front();
         que.pop();
-        set<Segment*> tmp_set = n_eplison_l(*front);
-        if(tmp_set.size() -1 >= minLins){
+        set<Segment*> tmp_set = n_eplison_l(front);
+        if(tmp_set.size() >= minLins){
             for(Segment *seg: tmp_set){
                 if(seg->getClusterId() == -1){
                     que.push(seg);
@@ -112,7 +116,6 @@ void expand_cluster(queue<Segment*> que,int cluster_id){
             }
         }
     }
-    cout<< "end  -------- "<<endl;
 }
 struct cmp{
     bool operator()( Point p1, Point p2){
@@ -127,19 +130,23 @@ struct cmp{
 
 void representative_trajectory_generation(){
     representive_point.resize(seg_global.size());
-    for(int i = 0; i < seg_global.size() ; i++){
 
-        int point_size = seg_global[i].size() * 2;
+    for(int i = 0; i < seg_global.size() ; i++){
+//        cout << "i: "<< i<< endl;
+        int point_size = (int)seg_global[i].size() * 2;
         Point *sort_point = new Point[point_size];
         Point *rep_point = new Point(0,0,-1),*zero_point = new Point(1,0,-1);
+
         for(int j = 0;j < seg_global[i].size();j++) {
             *rep_point = *rep_point + (seg_global[i][j]->getSegment().second - seg_global[i][j]->getSegment().first);
         }
         *rep_point = *rep_point / seg_global[i].size();
+
         double cos_theta = rep_point->dot(*zero_point) / rep_point->dist(Point(0,0,-1)); // cos(theta)
         double sin_theta = sqrt(1 - pow(cos_theta , 2)); // sin(theta)
 
         for(int j = 0;j < seg_global[i].size();j++){
+
             Point s =  seg_global[i][j]->getSegment().first,e =  seg_global[i][j]->getSegment().second;
             seg_global[i][j]->setSegment(Point(s.getX() * cos_theta + s.getY() * sin_theta, s.getY() * cos_theta - s.getX() * sin_theta, -1),
                                   Point(e.getX() * cos_theta + e.getY() * sin_theta, e.getY() * cos_theta - e.getX() * sin_theta, -1));
@@ -152,10 +159,13 @@ void representative_trajectory_generation(){
 //        for(int i=0;i<point_size;i++){
 //            cout << sort_point[i].getX() << " "<< sort_point[i].getY() << endl;
 //        }
-        for(int p=0; p < point_size * 2; p++){ //
+        for(int p=0; p < point_size; p++){ //
             int intersect_cnt = 0;
+
             Point *y = new Point(0, 0 , -1);
+
             for(int q =0 ;q < seg_global[i].size(); q++){
+//                cout << "q: "<< q<< endl;
                 Point s = seg_global[i][q]->getSegment().first,e = seg_global[i][q]->getSegment().second;
                 if(sort_point[p].getX() <= e.getX() && sort_point[p].getX() >= s.getX()){ // 在一个线段的中间
                     // 计算该点在线段上的点
@@ -175,14 +185,18 @@ void representative_trajectory_generation(){
             if(intersect_cnt >= minLins){ // 取均值
                 *y = *y / intersect_cnt;
                 Point *tmp = new Point(y->getX() * cos_theta - sin_theta * y->getY(), sin_theta * y->getX() + cos_theta * y->getY(),-1);
-                representive_point[i].push_back(tmp);
+//                cout<< tmp->getX() << " "<< tmp->getY()<<endl;
+                int size_point = representive_point[i].size() - 1; // 不让两点之间靠的太近
+                if(size_point< 0 || (size_point >= 0 && tmp->dist(*representive_point[i][size_point]) > min_dist))
+                    representive_point[i].push_back(tmp);
             }
-//            delete y;
+            delete y;
         }
-//        delete []sort_point;
-//        delete rep_point,zero_point;
+        delete []sort_point;
+        delete rep_point,zero_point;
     }
-    cout <<"representive_size : "<<representive_point.size() << endl;
+//    cout << "ending ........ "<< endl;
+//    cout <<"representive_size : "<<representive_point.size() << endl;
     for(int i=0;i<representive_point.size();i++){
         int len = ((int)representive_point[i].size()) - 1;
         for(int j = 0;j< len ;j++){
@@ -195,37 +209,36 @@ void representative_trajectory_generation(){
 }
 void line_segment_clustering(){
     int cluster_id = 0;
-    cout << "db_size: "<<partitionDB.size()<<endl;
+//    cout << "db_size: "<<partitionDB.size()<<endl;
     for (Segment *seg : partitionDB) {
         queue<Segment*> que;
         while(!que.empty()) que.pop();
         if (seg->getClusterId() == -1) { // 未被分类
-            set<Segment *> se = n_eplison_l(*seg);
-            if ((int)se.size() -1 >= minLins) { // include itself
+            set<Segment *> se = n_eplison_l(seg);
+            if ((int)se.size() >= minLins) { // include itself
+                seg->setClusterId(cluster_id);
                 for(Segment *seg1 : se){
                     seg1->setClusterId(cluster_id);
-                    if(seg1 != seg){
-                        que.push(seg1);
-                    }
                 }
                 expand_cluster(que, cluster_id);
                 cluster_id++;
             }
 
         }
-        cluster_cnt[seg->getClusterId()].insert(seg->getTrajId());
+        if(seg->getClusterId() != -1) {
+//            cout << "segment_id : " << seg->getClusterId() << endl;
+            cluster_cnt[seg->getClusterId()].insert(seg->getTrajId());
+        }
     }
     set<int> remove_cluster;
-    for (int i = 0; i < cluster_cnt.size(); i++) {
-        if (cluster_cnt[i].size() < minLins) {
+    int cluster_cnt_size = (int)cluster_cnt.size();
+    //cout << cluster_cnt_size <<" id  "<< cluster_id<< endl;
+
+    for (int i = 0; i < cluster_cnt_size ; i++) {
+        if (cluster_cnt[i].size() < min_traj) {
             remove_cluster.insert(i);
         }
     }
-    cout << "cluster_id : "<< cluster_id<<"remove_cluster: " << remove_cluster.size() << endl;
-//    map<int,set<int>> ::iterator it;
-//    for(it = cluster_cnt.begin();it != cluster_cnt.end() ;it++){
-//        cout << it->first<<endl;
-//    }
     seg_global.resize(cluster_id);
     int cluster_cnt = 0;
     map<int, int> map_cluster;
@@ -233,9 +246,9 @@ void line_segment_clustering(){
     try {
         for (Segment *tmp:partitionDB) {
             if (remove_cluster.find(tmp->getClusterId()) == remove_cluster.end()) {
-                cout << tmp->getSegment().first.getX() << " " << tmp->getSegment().first.getY() << " "
-                     << tmp->getSegment().second.getX() << " " << tmp->getSegment().second.getY() << " "
-                     << tmp->getClusterId() << endl;
+//                cout << tmp->getSegment().first.getX() << " " << tmp->getSegment().first.getY() << " "
+//                     << tmp->getSegment().second.getX() << " " << tmp->getSegment().second.getY() << " "
+//                     << tmp->getClusterId() << endl;
                 int id = tmp->getClusterId(); // cluster_id
                 if (map_cluster.find(id) == map_cluster.end()) {  // not in
                     map_cluster.insert(make_pair(id,cluster_cnt));
@@ -263,7 +276,7 @@ int main() {
 //    freopen("deer1995.tra","r",stdin);
 //    freopen("mini_data.in","r",stdin);
     freopen("hurricane1950_2006.tra","r",stdin);
-//    freopen("data.out","w",stdout);
+    freopen("data.out","w",stdout);
     cin>> dim >> traj_n;
     for(int i=0;i<traj_n;i++){
         int traj_id,point_num;
